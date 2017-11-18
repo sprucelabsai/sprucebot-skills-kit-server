fe29f74040b34205868fe89263da5760a2d48849
taylorromero <me>
2017-11-17 17:05:29 -0700

    logging

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+var debug = require('debug')('sprucebot-skills-kit-server')
 const config = require('config')
 
 module.exports = router => {
@@ -13,13 +14,18 @@ module.exports = router => {
 		let token =
 			id || ctx.cookies.get('jwt') || ctx.request.headers['x-skill-jwt']
 		if (token) {
+			debug(`middleware/auth found token at ${ctx.path}, checking`)
 			try {
 				var decoded = jwt.verify(token, config.API_KEY.toString().toLowerCase())
 				ctx.auth = await ctx.sb.user(decoded.locationId, decoded.userId)
 				ctx.auth.jwt = token
+				debug(`middleware/auth token valid at ${ctx.path}`)
 			} catch (err) {
+				debug(`middleware/auth token invalid at ${ctx.path}`)
 				ctx.throw('INVALID_AUTHENTICATION')
 			}
+		} else {
+			debug('middleware/auth no token found')
 		}
 
 		await next()
@@ -32,6 +38,7 @@ module.exports = router => {
 	router.use('/api/*/teammate/*', async (ctx, next) => {
 		let role = ctx.auth && ctx.auth.role
 		if (role !== 'teammate' && role !== 'owner') {
+			debug(`middleware/auth denied access to ${ctx.path} for role: '${role}' `)
 			ctx.throw('NOT_AUTHORIZED')
 		}
 		await next()
@@ -40,6 +47,7 @@ module.exports = router => {
 	router.use('/api/*/owner/*', async (ctx, next) => {
 		let role = ctx.auth && ctx.auth.role
 		if (role !== 'owner') {
+			debug(`middleware/auth denied access to ${ctx.path} for role: '${role}' `)
 			ctx.throw('NOT_AUTHORIZED')
 		}
 		await next()
@@ -48,6 +56,7 @@ module.exports = router => {
 	router.use('/api/*/guest/*', async (ctx, next) => {
 		let role = ctx.auth && ctx.auth.role
 		if (!role) {
+			debug(`middleware/auth denied access to ${ctx.path} for role: '${role}' `)
 			ctx.throw('NOT_AUTHORIZED')
 		}
 		await next()
