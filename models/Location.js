bfd7ab22328c71c5bf7069804a6636d6b3b1db6c
Shane McDermed <shmcdermed@gmail.com>
2018-03-05 15:15:21 -0700

    SB-889 - use correct syntax for sequelize>4 associations

--- models/Location.js
+++ models/Location.js
@@ -12,19 +12,6 @@ module.exports = (sequelize, DataTypes) => {
 			comment: 'The location name',
 			allowNull: true
 		},
-		openTime: {
-			type: DataTypes.TIME,
-			comment: 'Business open time',
-			allowNull: false,
-			defaultValue: '08:00:00'
-		},
-		closeTime: {
-			type: DataTypes.TIME,
-			comment: 'Business close time',
-			allowNull: false,
-			defaultValue: '20:00:00'
-		},
-		// https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 		timezone: {
 			type: DataTypes.STRING,
 			comment: 'The timezone',
@@ -61,43 +48,6 @@ module.exports = (sequelize, DataTypes) => {
 			comment: 'The country',
 			allowNull: true
 		},
-		geo: {
-			type: 'POINT',
-			comment: 'Users lng,lat point to get data in areas.',
-			get() {
-				const geoPoint = this.getDataValue('geo')
-				return geoPoint === null
-					? null
-					: {
-							lat: geoPoint.y,
-							lng: geoPoint.x
-						}
-			},
-			set(coords) {
-				if (coords === null) {
-					this.setDataValue('geo', null)
-				} else {
-					const lat = parseFloat(coords.lat)
-					const lng = parseFloat(coords.lng)
-					this.setDataValue(
-						'geo',
-						this.sequelize.literal(`POINT(${lng}, ${lat})`)
-					)
-				}
-			}
-		},
-		profileImageUUID: {
-			type: DataTypes.UUID,
-			comment:
-				'The base profile image uuid.  Used to build the filename for various profile image sizes.',
-			allowNull: true
-		},
-		profileImages: {
-			type: DataTypes.VIRTUAL,
-			get() {
-				return {}
-			}
-		},
 		isPublic: {
 			type: DataTypes.BOOLEAN,
 			comment: 'Is this location visible to the world?',
@@ -117,22 +67,19 @@ module.exports = (sequelize, DataTypes) => {
 					'addressState',
 					'addressZip',
 					'addressCountry',
-					'geo',
-					'OrganizationId',
-					'Organization',
-					'isConnected',
 					'isPublic',
 					'timezone'
 				]
 			}
-		},
-		classMethods: {
-			associate(models) {
-				this.hasMany(models.UserLocation, {
-					onDelete: 'cascade'
-				})
-			}
 		}
 	}
-	return sequelize.define(modelName, attributes, options)
+	const Location = sequelize.define(modelName, attributes, options)
+
+	Location.associate = function(models) {
+		this.hasMany(models.UserLocation, {
+			onDelete: 'cascade'
+		})
+	}
+
+	return Location
 }
